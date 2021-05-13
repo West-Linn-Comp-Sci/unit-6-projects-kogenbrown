b0e8ba3152368710515f645c3760d0dfa6add2b5
kogenbrown <kogenbrown@gmail.com>
2021-05-12 22:07:30 -0700

    Update Array2DExplorer.java

--- Arrays/Array2DExplorer.java
+++ Arrays/Array2DExplorer.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Array2DExplorer {
     public Array2DExplorer(){}
     public int minRow(int[][] nums, int row) {
@@ -29,15 +31,38 @@ public class Array2DExplorer {
     }
 
     public double[] averageCol(int[][] nums) {
-
+        double[] averages = new double[nums.length];
+        for(int i=0;i<nums[0].length;i++) {
+            double colTotal=0;
+            for(int j=0;nums.length;j++) {
+                colTotal+=nums[j][i];
+            }
+            averages[i]=colTotal/averages[0].length;
+        }
+        return averages;
     }
 
     public int smallEven(int[][] matrix) {
-
+        ArrayList<Integer> evens = new ArrayList<Integer>();
+        for(int i=0;i<matrix.length;i++) {
+            for(int n:matrix[i]) if(n%2==0) evens.add(n); 
+        }
+        int min = evens.get(0);
+        for(int n:evens) if(n<min) min = n;
+        return min;
     }
 
     public static int biggestRow(int[][] nums) {
-
+        int largestRowSum = 0, largestRowIndex = 0;
+        for(int i=0;i<nums.length;i++) {
+            int sum = 0;
+            for(int n:nums[i]) sum+=n;
+            if(sum>largestRowSum) {
+                largestRowSum=sum; 
+                largestRowIndex=i;
+            }
+        }
+        return largestRowIndex;
     }
 
 }
